4758cd2e063628a47a037a6a629cf65d1b4f0df8
František Bráblík <frantisek.brablik@gmail.com>
2022-08-01 03:02:08 +0200

    tea: Add dependency target

--- tea/tea.hs
+++ tea/tea.hs
@@ -9,7 +9,10 @@ import System.Environment ( getArgs )
 import System.Exit ( exitSuccess, exitFailure )
 import System.IO ( putStrLn )
 
+import System.Posix.User                        -- from package unix
+
 import System.Posix.Daemonize ( daemonize )     -- from package hdaemonize
+
 import DBus.Notify                              -- from package fdo-notify
     ( connectSession,
       notify,
@@ -25,7 +28,7 @@ notifAppName, notifHeading, notifContent, notifImage :: String
 notifAppName = "Teaman"
 notifHeading = "Teaman"
 notifContent = "Tea!"
-notifImage   = "~/.user-utils/tea/icon.png"
+notifImage   = {- HOME -} "/.user-utils/tea/icon.png"
 
 
 msToUs :: Num a => a -> a
@@ -37,10 +40,13 @@ theProgram mins = do
     -- wait given number of minutes
     threadDelay $ msToUs 1000 * 60 * mins
 
+    -- get home directory
+    home <- homeDirectory <$> (getRealUserID >>= getUserEntryForID)
+
     -- display system notification
     client <- connectSession
     let note = Note { appName = notifAppName
-                    , appImage = (Just . File) notifImage
+                    , appImage = (Just . File) (home ++ notifImage)
                     , summary = notifHeading
                     , body = (Just . Text) notifContent
                     , actions = []
@@ -55,11 +61,14 @@ main :: IO ()
 main = do
     args <- getArgs
 
+    -- home <- homeDirectory <$> (getRealUserID >>= getUserEntryForID)
+    -- putStrLn home
+
     let len = length args
-    when (len < 2)
+    when (len < 1)
         (putStrLn ("Invalid number of arguments: " ++ show len) >> exitFailure)
 
-    let param = readMaybe $ args !! 1 :: Maybe Int
+    let param = readMaybe $ args !! 0 :: Maybe Int
     case param of
         Nothing -> putStrLn "Invalid number" >> exitFailure
         Just mins -> daemonize $ theProgram mins
